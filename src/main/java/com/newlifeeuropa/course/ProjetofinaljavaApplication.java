3fd03cc84fc1c006e0cd6f39d117eec0eec54565
Damulertia <tefili1978@yahoo.com.br>
2021-12-05 17:41:52 -0300

    Project created

---
+++ src/main/java/com/newlifeeuropa/course/ProjetofinaljavaApplication.java
@@ -0,0 +1,13 @@
+package com.newlifeeuropa.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinaljavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinaljavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newlifeeuropa/course/ProjetofinaljavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newlifeeuropa.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinaljavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
