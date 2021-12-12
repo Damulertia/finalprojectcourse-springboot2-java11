278de2ef0f9d8543dcd7cc1a25b2cfbeae0b2a1e
Damulertia <tefili1978@yahoo.com.br>
2021-12-12 09:33:20 -0300

    Classes Category implementeds and tested

---
+++ src/main/java/com/newlifeeuropa/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.newlifeeuropa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.newlifeeuropa.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
